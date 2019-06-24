6a538d1f0c15c4df91361cf62432e10548131f39
Anna Oleksiak <anna.oleksiak@sollers.eu>
2019-06-24 12:15:49 +0200

    Completed module Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,38 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var countBetterStudents = 0;
+            foreach(var student in Students)
+            {
+                if(student.AverageGrade > averageGrade)
+                {
+                    countBetterStudents++;
+                }
+            }
+
+            var betterStudentsPercentage = (double)countBetterStudents / Students.Count * 100;
+
+            switch(betterStudentsPercentage)
+            {
+                case var _ when betterStudentsPercentage < 20.0:
+                    return 'A';
+                case var _ when betterStudentsPercentage < 40.0:
+                    return 'B';
+                case var _ when betterStudentsPercentage < 60.0:
+                    return 'C';
+                case var _ when betterStudentsPercentage < 80.0:
+                    return 'D';
+                default:
+                    return 'F';
+            }
+        }
     }
 }
